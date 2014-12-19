731de80821e744e4bc94d445c7f563d12a4511d1
Scarface2013 <mindscape11@gmail.com>
2014-12-18 20:11:31 -0500

    First Commit :D

--- README.txt
+++
@@ -1,3 +0,0 @@
-This is my first attempt at java game development.
-
-The object of the game is to shoot the bouncy arrow in the hole.
--- src/Apple.java
+++ src/Apple.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 
@@ -10,26 +11,66 @@ public class Apple extends JFrame {
 		Apple a = new Apple();
 		a.run(dm);
 	}
+	private Screen s;
+	private Image bg;
+	private Animation a;
+	
+	//loads pics from computer into java
+	public void loadPics(){
+		bg = new ImageIcon("C:\\Users\\Travis\\Desktop\\Programming\\eclipse\\Workstation\\GameOne\\Artwork\\Background1920.jpg").getImage();
+		Image CircleR = new ImageIcon("C:\\Users\\Travis\\Desktop\\Programming\\eclipse\\Workstation\\GameOne\\Artwork\\CircleRed.png").getImage();
+		Image CircleO = new ImageIcon("C:\\Users\\Travis\\Desktop\\Programming\\eclipse\\Workstation\\GameOne\\Artwork\\CircleOrange.png").getImage();
+		Image CircleY = new ImageIcon("C:\\Users\\Travis\\Desktop\\Programming\\eclipse\\Workstation\\GameOne\\Artwork\\CircleYellow.png").getImage();
+		Image CircleG = new ImageIcon("C:\\Users\\Travis\\Desktop\\Programming\\eclipse\\Workstation\\GameOne\\Artwork\\CircleGreen.png").getImage();
+		Image CircleC = new ImageIcon("C:\\Users\\Travis\\Desktop\\Programming\\eclipse\\Workstation\\GameOne\\Artwork\\CircleCyan.png").getImage();
+		Image CircleB = new ImageIcon("C:\\Users\\Travis\\Desktop\\Programming\\eclipse\\Workstation\\GameOne\\Artwork\\CircleBlue.png").getImage();
+		Image CircleV = new ImageIcon("C:\\Users\\Travis\\Desktop\\Programming\\eclipse\\Workstation\\GameOne\\Artwork\\CircleViolet.png").getImage();
+		Image CircleP = new ImageIcon("C:\\Users\\Travis\\Desktop\\Programming\\eclipse\\Workstation\\GameOne\\Artwork\\CirclePink.png").getImage();
+		a = new Animation();
+		a.addScene(CircleR,250);
+		a.addScene(CircleO,250);
+		a.addScene(CircleY,250);
+		a.addScene(CircleG,250);
+		a.addScene(CircleC,250);
+		a.addScene(CircleB,250);
+		a.addScene(CircleV,250);
+		a.addScene(CircleP,250);
+	}
+	
+	//main engine
 	public void run(DisplayMode dm){
-		setBackground(Color.PINK);
-		setForeground(Color.WHITE);
-		setFont(new Font("Arial", Font.PLAIN, 24));
-		
-		Screen s = new Screen();
+		s = new Screen();
 		try{
-			s.setFullScreen(dm, this);
-			try{
-				Thread.sleep(5000);
-				}catch(Exception ex){}
+			s.setFullScreen(dm, new JFrame());
+			loadPics();
+			movieLoop();
 		}finally{
 			s.restoreScreen();
 		}
 	}
-	public void paint(Graphics g){
-		if(g instanceof Graphics2D){
-			Graphics2D g2 = (Graphics2D)g;
-			g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+	
+	//main movie loop
+	public void movieLoop(){
+		long startingTime = System.currentTimeMillis();
+		long cumTime = startingTime;
+		
+		while(cumTime - startingTime < 5000){
+			 long timePassed = System.currentTimeMillis() - cumTime;
+			 cumTime += timePassed;
+			 a.update(timePassed);
+			 
+			 Graphics g = s.getFullScreenWindow().getGraphics();
+			 draw(g);
+			 g.dispose();
+			 
+			 try{
+				 Thread.sleep(20);
+			 }catch(Exception ex){}
 		}
-		g.drawString("This is Gonna Be awesome", 200, 200);
+	}
+	
+	public void draw(Graphics g){
+		g.drawImage(bg,0,0,null);
+		g.drawImage(a.getImage(),600,600,null);
 	}
 }
